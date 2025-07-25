f98bda34c4d7d70d81389d60f03be3a3856ddbac
Manu Yadav <manukumaryadav769@gmail.com>
2025-07-25 12:45:39 +0530

    Debugging

---
+++ models/listing.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+title: {
+    type: String,
+    required: true,
+},
+description: String,
+image: {
+    type: String,
+    default: "https://www.pexels.com/photo/brown-wooden-dock-414612/",
+    set: (v) =>
+        v === ""
+            ? "https://www.pexels.com/photo/brown-wooden-dock-414612/"
+            : v,
+},
+price:Number,
+location:String,
+country:String,
+reviews: [
+    {
+        type: Schema.Types.ObjectId,
+        ref: "Review",
+    },
+],
+});
+const Listing = mongoose.model("Listing", listingSchema);
+
+module.exports = Listing;
