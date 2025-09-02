933ff9cda4ebb15f1782c634302423a5eeb0a31f
Manu Yadav <manukumaryadav769@gmail.com>
2025-09-02 14:47:54 +0530

    this is commit

--- routes/user.js
+++ routes/user.js
@@ -8,7 +8,7 @@ const {saveRedirectUrl}=require("../middleware.js");
 const userController = require("../controllers/users.js");
 
 router
-.route("/")
+.route("/signup")
 .get(userController.renderSignupForm)
 .post(wrapasync(userController.signup));
 
