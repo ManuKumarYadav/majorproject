85902d76cfe27d5f48e021ee66f1e37ff64b8ffe
Manu Yadav <manukumaryadav769@gmail.com>
2025-09-02 14:31:56 +0530

    This is a Second Commit

--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,10 @@ const {saveRedirectUrl}=require("../middleware.js");
 
 const userController = require("../controllers/users.js");
 
+router.get("/", (req, res)=>{
+    res.send("This is User Home Page");
+})
+
 router
 .route("/signup")
 .get(userController.renderSignupForm)
